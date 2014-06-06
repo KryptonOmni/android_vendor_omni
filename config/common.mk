TeamEOS/vendor_eos

Add Supersu to builds
Change-Id: I44216ec377a1a21a7064996e1ef1c202a7f99dba

config/common.mk
@@ -77,6 +77,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES +=  \
     vendor/eos/prebuilt/common/etc/permissions/com.playstation.playstationcertified.xml:system/etc/permissions/com.playstation.playstationcertified.xml
 
+# Supersu support
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/apk/Superuser.apk:system/app/Superuser.apk \
+    vendor/eos/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+    vendor/eos/prebuilt/common/xbin/daemonsu:system/xbin/daemonsu \
+    vendor/eos/prebuilt/common/xbin/su:system/xbin/su
+    
 # Required eos packages
 PRODUCT_PACKAGES += \
     BluetoothExt \

KryptonOmni/android_vendor_omni

Conflicts:
	config/common.mk

config/common.mk
@@ -51,6 +51,14 @@ PRODUCT_COPY_FILES += \
 # Init script file with omni extras
 PRODUCT_COPY_FILES += \
     vendor/omni/prebuilt/etc/init.local.rc:root/init.omni.rc
+    
+# Supersu support
+PRODUCT_COPY_FILES += \
+    vendor/omni/prebuilt/common/apk/Superuser.apk:system/app/Superuser.apk \
+    vendor/omni/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+    vendor/omni/prebuilt/common/xbin/daemonsu:system/xbin/daemonsu \
+    vendor/omni/prebuilt/common/bin/99-supersu.sh:system/addon.d/99-supersu.sh \
+    vendor/omni/prebuilt/common/xbin/su:system/xbin/su
 
 # Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \